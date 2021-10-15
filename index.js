026f3cfdb834e1783def058f5693d645e976992a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-10-15 16:15:44 -0700

    have to read more carefully put passed test

--- index.js
+++ index.js
@@ -162,10 +162,10 @@ function greeting(firstName, lastName){
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(length, width, height){
-  this.length = length;
-  this.width = width;
-  this.height = height;
+function CuboidMaker(object){
+  this.length = object.length;
+  this.width = object.width;
+  this.height = object.height;
 }
 
 // let instance = new CuboidMaker(10,5, 25);
